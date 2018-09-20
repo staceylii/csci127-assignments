1b5cf471a55e23701a36661c3b20bf7e07468834
staceylii <listacey22@yahoo.com>
2018-09-19 20:13:49 -0400

    update

--- hw_02/pig.py
+++ hw_02/pig.py
@@ -1,20 +1,16 @@
 """
 Stacey Li
 Jade Baptiste
-""""
+"""
 
 
 def part_pig_latin(name):
-    """
-    Input: A string that is a single lower case word
-    Returns: that string in fake pig latin -> move the first letter of the word to the end and add "ay"
-    so: "hello" --> "ellohay"
-    """
-    vowel = 'aeiou'
-    if name[0] not in vowel:
-        x = name[1:]
-        return x + name[0] + "ay"
+    vowels = ('a', 'e', 'i', 'o', 'u',)
+    x = name[0]
+    if x.startswith(vowels):
+            return name + "ay"
     else:
-        return name
+            return name[1:] + x + "ay"
+
     
 print (part_pig_latin ("horace"))
